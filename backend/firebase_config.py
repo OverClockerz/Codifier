297d2bedb0ee8425a4988fa91473f5a59ef5780c
unknown <sssdhamaka@gmail.com>
2025-12-21 13:56:05 +0530

    setting up

--- backend/firebase_config.py
+++ backend/firebase_config.py
@@ -4,5 +4,5 @@ from firebase_admin import credentials, db
 def initialize_firebase():
     cred = credentials.Certificate('serviceAccountKey.json') 
     firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://social-flow-423402-default-rtdb.firebaseio.com/' 
+        'databaseURL': 'https://codifiergit-00515167-c4af4-default-rtdb.asia-southeast1.firebasedatabase.app/' 
     })
