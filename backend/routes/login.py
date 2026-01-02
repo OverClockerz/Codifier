6cc87af36f25312238041577ba6b42bc61677f5e
Pushkar Pan <pushkarpan03@gmail.com>
2026-01-03 02:23:00 +0530

    Delete backend directory

--- backend/routes/login.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, request, redirect, url_for,session
-
-login_bp = Blueprint('login', __name__)
-
-@login_bp.route("/login", methods=["GET","POST"])
-def login():
-    return render_template("login.html")
