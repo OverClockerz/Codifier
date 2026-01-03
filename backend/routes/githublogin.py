effada5b9be9b06bc1fab7b24071926ba972144e
Raziel206 <chatterjeedevauydh.2006@gmail.com>
2026-01-03 21:02:53 +0530

    yoohoooo

--- backend/routes/githublogin.py
+++ backend/routes/githublogin.py
@@ -76,5 +76,5 @@ def github_callback():
         mongo.db.players.insert_one(player)
 
     return redirect(
-        f"https://office-07701907-eeb02.web.app//?username={username}"
+        f"https://office-2fcd2--dev-0p9x0aa4.web.app/?username={username}"
     )  
