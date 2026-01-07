6d64bf9d9dbbfb68fb267216648dd442803c03ab
Raziel206 <chatterjeedevauydh.2006@gmail.com>
2026-01-08 05:05:23 +0530

    cors

--- backend/main.py
+++ backend/main.py
@@ -29,20 +29,12 @@ mongo.init_app(app)
 # Vital for React + Flask Session Cookies
 CORS(
     app,
-    resources={
-        r"/*": {
-            "origins": [
-                "http://localhost:3000",
-                "https://office-2fcd2.web.app"
-            ]
-        }
-    },
+    resources={r"/*": {"origins": "https://office-2fcd2.web.app"}},
     supports_credentials=True,
     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["Content-Type", "Authorization", "X-Requested-With"],
     expose_headers=["Content-Type"]
 )
-
 
 
 # Register Active Blueprints
